2fbb67af206563e9a9e3e59c95b56c938ad3feeb
eayuban <edraelan.ayuban@ucalgary.ca>
2016-11-11 22:28:31 -0700

    "coWorkers" hashSet data structure, "addCoWorker" method and "hasCoWorker" method
    Implemented a new data structure named 'coWorkers' that contains the names of all the people that this person works with. Also implemented methods to add co-workers and to check if this person works with a certain person.

--- src/cpsc433/Person.java
+++ src/cpsc433/Person.java
@@ -1,16 +1,19 @@
 package cpsc433;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Person {
 	private String name;
 	public boolean smokes;
 	public boolean hacks;
 	private ArrayList<String> roles;
+	private HashSet<String> coWorkers;
 	
 	public Person(String name){
 		this.name = name;
 		roles = new ArrayList<String>();
+		coWorkers = new Set<String>();
 	}
 	
 	public String getName(){
@@ -23,12 +26,22 @@ public class Person {
 		}
 	}
 	
-	public boolean hasRole(String role){	// This function returns true if this person has the role specified by the argument
+	public boolean hasRole(String role){	// This method returns true if this person has the role specified by the argument
 		if(roles.contains(role)){	// given. 'role' should be one of "secretary", "manager", or "researcher".
 			return true;
 		}
 		else{
 			return false;
 		}
+	}
+	
+	// This method accepts a person 'p' and adds them to the 'coWorker' Set via the person's name. Since this is a HashSet, it
+	// will only add the person's name if it is not already in the set.
+	public void addCoWorker(Person p){
+		coWorkers.add(p.getName());
+	}
+	
+	public boolean hasCoWorker(String p){
+		return coWorkers.contains(p);
 	}
 }
