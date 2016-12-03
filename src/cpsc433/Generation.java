2d47896edf2ee5b95f5bfc09ef0c5d6c905da448
eayuban <edraelan.ayuban@ucalgary.ca>
2016-12-03 16:06:29 -0700

    Added support methods for 'cullGeneration'

--- src/cpsc433/Generation.java
+++ src/cpsc433/Generation.java
@@ -20,7 +20,11 @@ public class Generation {
 		nodeN.putStrings(b);
 		this.facts.add(nodeN);
 	}
-	
+
+	public void addFact(Node n){
+		this.facts.add(n);
+	}	
+
 	public Node bestNode(){
 		Iterator<Node> nodes = facts.iterator();
 		Node maxNode = null;
@@ -34,6 +38,54 @@ public class Generation {
 			}	
 		}
 		return maxNode;
+	}
+		
+	/**
+	 * A function that returns an Array filled with Nodes that are the top
+	 * percentile of the generation
+	 *
+	 * @param	percentage	The portion of the best nodes in a generation
+	 *						you want returned in the array
+	 *
+	 * @return	best		The array containing 
+	 */
+	public Node[] getBestOfGen(float percentage){
+		Node[] best = new Node[Math.round(facts.size() * percentage)];
+		
+		Node[] sorted = sortGen();
+		
+		int j = facts.size() - 1;
+		for (int i =0; i < best.length; i++){
+			best[i] = sorted[j];
+			j--;
+		}
+		
+		return best;
+	}
+		
+	public Node[] getWorstOfGen(float percentage){
+		Node[] worst = new Node[Math.round(facts.size() * percentage)];
+		
+		Node[] sorted = sortGen();
+		
+		for (int i =0; i < worst.length; i++){
+			worst[i] = sorted[i];
+		}
+		
+		return worst;
+	}
+	
+	private Node[] sortGen(){
+		Node[] nArray = new Node[facts.size()];
+		Object[] oArray = facts.toArray();
+		
+		for (int i = 0; i < oArray.length; i++){
+			nArray[i] = (Node) oArray[i];
+		}
+		
+		NodeQuickSort.sort(nArray, 0, nArray.length - 1);
+				
+		return nArray;
 	}
 		
 	@Override
